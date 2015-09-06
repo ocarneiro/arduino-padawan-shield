54c9a74fed8586f3bfced3215beb25ef7c3069b2
Otavio Carneiro <otavio@carneiro.blog.br>
2015-09-06 20:42:31 -0300

    Potenciometro e Display 7 seg

--- padawan/padawan.ino
+++ padawan/padawan.ino
@@ -19,13 +19,27 @@ int potenciometro = A2;
 int pinoDisplay[TAMANHO_DISPLAY] = {3,5,6,7,8,A4,A5};
 
 void setup() {
-
+  //inicializa potenciometro
+  pinMode(potenciometro, INPUT);
+  
+  //inicializa os LED's do display de 7 segmentos
   for (int item = 0; item < TAMANHO_DISPLAY; item++) {
     pinMode(pinoDisplay[item], OUTPUT);
   }
 }
 
 void loop() {
+  int valorPot = analogRead(potenciometro);
+  int segmentoAMostrar = map(valorPot,0,255,0,8);
+    
+  digitalWrite(pinoDisplay[segmentoAMostrar], HIGH);
+  delay (200);
+  digitalWrite(pinoDisplay[segmentoAMostrar], LOW);
+  
+}
+  
+void test7segmentsDisplay() {
+
   for (int item = 0; item < TAMANHO_DISPLAY; item++) {
     digitalWrite(pinoDisplay[item], HIGH);
     delay(500);

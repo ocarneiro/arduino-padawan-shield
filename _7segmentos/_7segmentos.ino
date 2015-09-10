2fdc20be14ad3d21b60cd9840734b4e117084175
Otavio Carneiro <otaviocsantos@gmail.com>
2015-09-09 23:31:33 -0300

    otimizacao de codigo

--- _7segmentos/_7segmentos.ino
+++ _7segmentos/_7segmentos.ino
@@ -2,15 +2,10 @@
 #define TAMANHO_DISPLAY 7
 const int pinoDisplay[TAMANHO_DISPLAY] = {A4,A5,7,8,5,3,6};
 
-const int display0[TAMANHO_DISPLAY] = {1,1,1,1,1,1,0};
-const int display1[TAMANHO_DISPLAY] = {0,1,1,0,0,0,0};
-
-/*
 const int numeros[10][TAMANHO_DISPLAY] = {
   {1,1,1,1,1,1,0},
   {0,1,1,0,0,0,0}
 };
-*/
 
 void setup() {
   //inicializa os LED's do display de 7 segmentos
@@ -20,21 +15,13 @@ void setup() {
 }
 
 void loop() {
-  for (int seg = 0; seg < TAMANHO_DISPLAY; seg++) {
-    digitalWrite(pinoDisplay[seg], display1[seg]);
-  }
+  for (int numero_atual = 0; numero_atual < 2; numero_atual++) {
+    for (int seg = 0; seg < TAMANHO_DISPLAY; seg++) {
+      digitalWrite(pinoDisplay[seg], numeros[numero_atual][seg]);
+    }
   delay(1000);
-  for (int seg = 0; seg < TAMANHO_DISPLAY; seg++) {
-    digitalWrite(pinoDisplay[seg], LOW);
   }
-  delay(1000);
-  for (int seg = 0; seg < TAMANHO_DISPLAY; seg++) {
-    digitalWrite(pinoDisplay[seg], display0[seg]);
-  }
-  delay(1000);
-  for (int seg = 0; seg < TAMANHO_DISPLAY; seg++) {
-    digitalWrite(pinoDisplay[seg], LOW);
-  }
+  
 }
 
 

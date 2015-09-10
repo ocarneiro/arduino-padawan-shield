7c26bc4cb57c024e97a3fb59569b16b45c4bbe95
Otavio Carneiro <otaviocsantos@gmail.com>
2015-09-09 23:34:41 -0300

    Contagem de 0 a 9

--- _7segmentos/_7segmentos.ino
+++ _7segmentos/_7segmentos.ino
@@ -3,8 +3,16 @@
 const int pinoDisplay[TAMANHO_DISPLAY] = {A4,A5,7,8,5,3,6};
 
 const int numeros[10][TAMANHO_DISPLAY] = {
-  {1,1,1,1,1,1,0},
-  {0,1,1,0,0,0,0}
+  {1,1,1,1,1,1,0},  //0
+  {0,1,1,0,0,0,0},  //1
+  {1,1,0,1,1,0,1},  //2
+  {1,1,1,1,0,0,1},  //3
+  {0,1,1,0,0,1,1},  //4
+  {1,0,1,1,0,1,1},  //5
+  {0,0,1,1,1,1,1},  //6
+  {1,1,1,0,0,0,0},  //7
+  {1,1,1,1,1,1,1},  //8
+  {1,1,1,0,0,1,1}
 };
 
 void setup() {
@@ -15,7 +23,7 @@ void setup() {
 }
 
 void loop() {
-  for (int numero_atual = 0; numero_atual < 2; numero_atual++) {
+  for (int numero_atual = 0; numero_atual < 10; numero_atual++) {
     for (int seg = 0; seg < TAMANHO_DISPLAY; seg++) {
       digitalWrite(pinoDisplay[seg], numeros[numero_atual][seg]);
     }

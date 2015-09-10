277171da3f158762eb80465800f77c60d0ed2e9b
Otavio Carneiro <otaviocsantos@gmail.com>
2015-09-09 22:43:28 -0300

    modos de operacao

--- padawan/padawan.ino
+++ padawan/padawan.ino
@@ -14,14 +14,21 @@
  https://github.com/ocarneiro/arduino-padawan-shield
  */
 
-int potenciometro = A2;
+const int botao = 2;
+const int potenciometro = A2;
 #define TAMANHO_DISPLAY 7
-int pinoDisplay[TAMANHO_DISPLAY] = {3,5,6,7,8,A4,A5};
+const int pinoDisplay[TAMANHO_DISPLAY] = {3,5,6,7,8,A4,A5};
+
+//modos
+const int modos = 3;
+const int MODO_1 = 1;
+const int MODO_2 = 2;
+
+int modoAtual = 0;
 
 void setup() {
-  //inicializa potenciometro
-  pinMode(potenciometro, INPUT);
-  
+  //inicializa botao
+  pinMode(botao, INPUT);
   //inicializa os LED's do display de 7 segmentos
   for (int item = 0; item < TAMANHO_DISPLAY; item++) {
     pinMode(pinoDisplay[item], OUTPUT);
@@ -29,13 +36,31 @@ void setup() {
 }
 
 void loop() {
+  while(modoAtual < modos) {
+    if(modoAtual == MODO_1) {
+      modo1();
+    } else if(modoAtual == MODO_2) {
+      modo2();
+    }
+    if(digitalRead(botao) == HIGH) {
+      modoAtual = modoAtual + 1;
+      delay(250);
+    }
+  }
+  modoAtual = 0;
+  delay(500);
+}
+
+void modo1() {
   int valorPot = analogRead(potenciometro);
   int segmentoAMostrar = map(valorPot,0,255,0,8);
     
   digitalWrite(pinoDisplay[segmentoAMostrar], HIGH);
   delay (200);
   digitalWrite(pinoDisplay[segmentoAMostrar], LOW);
-  
+}
+
+void modo2() {
 }
   
 void test7segmentsDisplay() {
